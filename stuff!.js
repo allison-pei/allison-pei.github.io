f17a09dcbcc779c2acd28edc62a853b8f6d7804d
allisonpei1 <36796680+allisonpei1@users.noreply.github.com>
2018-07-31 14:27:08 -0700

    second commit

---
+++ stuff!.js
@@ -0,0 +1,9 @@
+$(window).load(function(){
+        $(".typing").typed({
+    strings:["from ml to md"],
+    typeSpeed: 10,
+    backDelay: 2500,
+    loop: !0, // here
+    startDelay: 1000,
+});
+});
